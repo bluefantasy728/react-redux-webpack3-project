250ed42c40d6e27153cf9747652573896f37a422
bluefantasy728 <jiangguoxi728@hotmail.com>
2017-07-23 17:23:21 +0800

    update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,51 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as userInfoActionFromOtherFile from '../actions/userInfo.js'
 
-export default class App extends React.Component {
+class App extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+      initDone: false
+    }
+  }
+  componentDidMount() {
+    console.log(this.props.userInfoAction.updateInfo)
+    this.props.userInfoAction.updateInfo({
+      cityName: 'Shanghai'
+    })
+
+    setTimeout(() => {
+      this.setState({
+        initDone: true
+      })
+      console.log(this.props)
+    }, 2000);
+  }
   render() {
     return (
       <div>
         App
-        {this.props.children}
+        {
+          this.state.initDone
+          ? this.props.children
+          : <div>正在加载中...</div>
+        }
       </div>
     )
   }
 }
+const mapState = (state) => {
+  return {
+  }
+}
+const mapDispatch = (dispatch) => {
+  return {
+    userInfoAction: bindActionCreators(userInfoActionFromOtherFile, dispatch)
+  }
+}
+export default connect(
+  mapState,
+  mapDispatch
+)(App)
